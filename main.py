ef151b129bf867746c94b94ad2929c88dbf4719f
bharathreddypanyala <55359052+bharathreddypanyala@users.noreply.github.com>
2020-01-03 15:40:33 +0100

    main.py for multiplication works

--- main.py
+++ main.py
@@ -7,41 +7,7 @@ app = Flask(__name__)
 def index():
     return 'Usage;\nOperation?A=<Value1>&B=<Value2>\n'
 
-@app.route('/add')
-def addition():
-    try:
-        val1=request.args.get('A',default = 0, type = Fraction)
-    except ZeroDivisionError as error:
-        val1='None'
-    try:
-        val2=request.args.get('B',default = 0, type = Fraction)
-    except ZeroDivisionError as error:
-        val2='None'
-    if val1 == 'None' or val2 == 'None' :
-        return 'None'
-    else:
-        X = Fraction(val1)
-        Y = Fraction(val2)
-        result= X+Y
-        return str(round(float(result),4))
 
-@app.route('/sub')
-def subtraction():
-    try:
-        val1=request.args.get('A',default = 0, type = Fraction)
-    except ZeroDivisionError as error:
-        val1='None'
-    try:
-        val2=request.args.get('B',default = 0, type = Fraction)
-    except ZeroDivisionError as error:
-        val2='None'
-    if val1 == 'None' or val2 == 'None' :
-        return 'None'
-    else:
-        X = Fraction(val1)
-        Y = Fraction(val2)
-        result= X-Y
-        return(str(round(float(result),4)))
 
 
 @app.route('/mul')
@@ -62,26 +28,7 @@ def multiplication():
         result= X*Y
         return(str(round(float(result),4)))
 
-@app.route('/div')
-def division():
-    try:
-        val1=request.args.get('A',default = 0, type = Fraction)
-    except ZeroDivisionError as error:
-        val1='None'
-    try:
-        val2=request.args.get('B',default = 0, type = Fraction)
-    except ZeroDivisionError as error:
-        val2='None'
-    if val1 == 'None' or val2 == 'None' :
-        return 'None'
-    else:
-        X = Fraction(val1)
-        Y = Fraction(val2)
-        try:
-            result= X/Y
-            return(str(round(float(result),4)))
-        except ZeroDivisionError as error:
-            return 'None'
+
 
 if __name__ == "__main__":
     app.run()
